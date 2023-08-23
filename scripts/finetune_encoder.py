c6168395c5b1bc2c8b96d5e3a1600dfec84ffb22
Giorgioskij <giorgiopuntostrano@gmail.com>
2023-08-23 17:14:37 +0200

    minor fix. now finetuning

--- scripts/finetune_encoder.py
+++ scripts/finetune_encoder.py
@@ -5,8 +5,9 @@
     Training 2023-08-23 17:12
 """
 import sys
+from pathlib import Path
 
-sys.path.append("../")
+sys.path.append(str(Path(__file__).parent.parent))
 
 import config as cfg
 from main import train
