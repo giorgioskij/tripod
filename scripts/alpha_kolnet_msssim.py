9b856bd4861e570014c5b67d70d7b75f06508bcf
Giorgioskij <giorgiopuntostrano@gmail.com>
2023-09-09 19:53:43 +0200

    started training with patches of 1024 pixels

---
+++ scripts/alpha_kolnet_msssim.py
@@ -0,0 +1,37 @@
+"""
+    TARGET:         decoder only (encoder pretrained on imagenet)
+    PATCHES:        256 pixels (minimum for ms_ssim is 161)
+    LOSS:           MS_SSIM only
+    INTENSITY:      0.2 
+    EPOCHS:         250
+    PREPROCESSING:  crop, flip and rotate
+
+    Training 2023-09-08 12:00
+"""
+
+import script_config
+import loss
+from main import train
+import preprocessing
+
+preprocessor = preprocessing.Unsharpen(patch_size=256,
+                                       max_amount=0.2,
+                                       rotate=True)
+
+loss_fn = loss.MS_SSIMLoss()
+
+model_args = {
+    "loss_fn": loss_fn,
+    "learning_rate": 1e-3,
+    "freeze_encoder": True,
+    "use_espcn": True,
+    "use_espcn_activations": True,
+    "avoid_deconv": True,
+    "use_alpha": True,
+    "double_image_size": False
+}
+
+train(model_args=model_args,
+      preprocessor=preprocessor,
+      n_epochs=250,
+      run_name="alpha_perceptual_kolnet_msssim")
---
+++ scripts/alpha_kolnet_ssim_1024.py
@@ -0,0 +1,37 @@
+"""
+    TARGET:         Entire model, starting from finetuned on 256 pixel images
+    PATCHES:        1024 pixels 
+    LOSS:           SSIM (non-negative) only
+    INTENSITY:      0.15
+    EPOCHS:         500. Already done 250 training + 500 finetune + 500 512pixels
+    PREPROCESSING:  crop, flip and rotate
+    LEARNING RATE:  4e-5
+
+    Training 2023-09-09 19:53
+"""
+
+import script_config
+import loss
+from main import train
+import preprocessing
+import config as cfg
+from uresnet import UResNet
+
+preprocessor = preprocessing.Unsharpen(patch_size=1024,
+                                       max_amount=0.15,
+                                       rotate=True)
+
+loss_fn = loss.SSIMLoss()
+
+model = UResNet.load_from_checkpoint(cfg.CKP_PATH / "alpha_kolnet_ssim_512" /
+                                     "epoch=323-valid_loss=0.013.ckpt",
+                                     learning_rate=4e-5)
+
+train(
+    model=model,
+    preprocessor=preprocessor,
+    n_epochs=500,
+    run_name="alpha_kolnet_ssim_1024",
+    batch_size_train=4,
+    batch_size_test=8,
+)
